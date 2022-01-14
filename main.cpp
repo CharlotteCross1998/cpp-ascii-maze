d2406ed171e064075f76eb6590d6aba8e774d366
charlottecross1998 <29734551+CharlotteCross1998@users.noreply.github.com>
2022-01-14 01:24:47 +0000

    Improved code. Changed to ncurses.

--- main.cpp
+++ main.cpp
@@ -1,15 +1,8 @@
-#ifdef _WIN32
-	#define CLEAR "cls"
-#else 
-	#define CLEAR "clear"
-#endif
-
 #include <iostream>
 #include <limits>
+#include <ncurses.h>
 
 const bool DEBUG = false;
-
-using namespace std;
 
 const int MAZE_X = 9, MAZE_Y = 9;
 int playerX = 1, playerY = 8;
@@ -42,27 +35,26 @@ char maze[MAZE_Y][MAZE_X] = {
 	'#',' ' ,'#','#' ,'#','#','#' ,' ' ,'#'
 };
 
-int InitialiseMaze()
+bool InitialiseMaze()
 {
-	int success = 0;
 	if(maze[playerY][playerX] != ' ') 
 	{
-		cout << "Error placing player... not in an empty tile!\n";
-		success = -1;	
+		printw("Error placing player... not in an empty tile!\n");
+		return false;
 	}
 	else maze[playerY][playerX] = PLAYER_SYMBOL;
 	if(maze[goalY][goalX] != ' ')
 	{		
-		cout << "Error placing goal... not in an empty tile!\n";
-		success = -1;
+		printw("Error placing goal... not in an empty tile!\n");
+		return false;
 	}	
 	else maze[goalY][goalX] = GOAL_SYMBOL;
-	return success;
+	return true;
 }
 
 void UpdatePlayerLocation(int y, int x)
 {
-	if(DEBUG) cout << "Old player: y=" << playerY << " x=" << playerX << "\nNew player: y=" << y << " x=" << x << "\n";
+	if(DEBUG) printw("Old player: y=%d x=%d\nNew player: y=%d x=%d\n", playerY, playerX, y, x);
 	maze[playerY][playerX] = ' ';
 	maze[y][x] = PLAYER_SYMBOL;
 	playerY = y;
@@ -71,88 +63,81 @@ void UpdatePlayerLocation(int y, int x)
 
 void HandleInput()
 {
-	char ch;
-	cin >> ch;
-	if(cin.fail())
+	char ch = getch();
+	switch(ch)
 	{
-		cin.clear();
-		cin.ignore(numeric_limits<streamsize>::max(), '\n'); //clear and reset
-	}
-	else
-	{
-		switch(ch)
-		{
-			case 'w':
-				if(maze[playerY - 1][playerX] == ' ' || maze[playerY - 1][playerX] == GOAL_SYMBOL) UpdatePlayerLocation(playerY - 1, playerX);
-				else
-				{
-					if(DEBUG) cout << "Can't go from [" << playerY << "," << playerX << "] to [" << playerY - 1 << "," << playerX << "]\nValue in [" << playerY - 1 << "," << playerX << "]: " << maze[playerY -1][playerX] << "\n";
-				}
-				break;
-			case 'a':
-				if(maze[playerY][playerX - 1] == ' ' || maze[playerY][playerX - 1] == GOAL_SYMBOL) UpdatePlayerLocation(playerY, playerX - 1);
-				else
-				{
-					if(DEBUG) cout << "Can't go from [" << playerY << "," << playerX << "] to [" << playerY << "," << playerX - 1 << "]\nValue in [" << playerY << "," << playerX - 1<< "]: " << maze[playerY][playerX - 1] << "\n";
-				}
-				break;
-			case 's':
-				if(maze[playerY + 1][playerX] == ' ' || maze[playerY + 1][playerX] == GOAL_SYMBOL) UpdatePlayerLocation(playerY + 1, playerX);
-				else
-				{
-					if(DEBUG) cout << "Can't go from [" << playerY << "," << playerX << "] to [" << playerY + 1 << "," << playerX << "]\nValue in [" << playerY + 1 << "," << playerX << "]: " << maze[playerY +1][playerX] << "\n";
-				}
-				break;
-			case 'd':
-				if(maze[playerY][playerX + 1] == ' ' || maze[playerY][playerX + 1] == GOAL_SYMBOL) UpdatePlayerLocation(playerY, playerX + 1);
-				else
-				{
-					if(DEBUG) cout << "Can't go from [" << playerY << "," << playerX << "] to [" << playerY << "," << playerX + 1 << "]\nValue in [" << playerY << "," << playerX + 1 << "]: " << maze[playerY][playerX+1] << "\n";
-				}
-				break;
-		}
+		case 'w':
+			if(maze[playerY - 1][playerX] == ' ' || maze[playerY - 1][playerX] == GOAL_SYMBOL) UpdatePlayerLocation(playerY - 1, playerX);
+			else
+			{
+				if(DEBUG) printw("Can't go from [%d,%d] to [%d,%d]\nValue in [%d,%d]: %c\n", playerY, playerX, playerY - 1, playerX, playerY - 1, playerX, maze[playerY - 1][playerX]);
+			}
+			break;
+		case 'a':
+			if(maze[playerY][playerX - 1] == ' ' || maze[playerY][playerX - 1] == GOAL_SYMBOL) UpdatePlayerLocation(playerY, playerX - 1);
+			else
+			{
+				if(DEBUG) printw("Can't go from [%d,%d] to [%d,%d]\nValue in [%d,%d]: %c\n", playerY, playerX, playerY, playerX - 1, playerY, playerX - 1, maze[playerY][playerX - 1]);
+			}
+			break;
+		case 's':
+			if(maze[playerY + 1][playerX] == ' ' || maze[playerY + 1][playerX] == GOAL_SYMBOL) UpdatePlayerLocation(playerY + 1, playerX);
+			else
+			{
+				if(DEBUG) printw("Can't go from [%d,%d] to [%d,%d]\nValue in [%d,%d]: %c\n", playerY, playerX, playerY + 1, playerX, playerY + 1, playerX, maze[playerY + 1][playerX]);
+			}
+			break;
+		case 'd':
+			if(maze[playerY][playerX + 1] == ' ' || maze[playerY][playerX + 1] == GOAL_SYMBOL) UpdatePlayerLocation(playerY, playerX + 1);
+			else
+			{
+				if(DEBUG) printw("Can't go from [%d,%d] to [%d,%d]\nValue in [%d,%d]: %c\n", playerY, playerX, playerY, playerX + 1, playerY, playerX + 1, maze[playerY][playerX + 1]);
+			}
+			break;
 	}
 }
 
-int CheckState()
+bool CheckState()
 {
-	if(playerX == goalX && playerY == goalY) return 2;
-	return 1;
+	if(playerX == goalX && playerY == goalY) return true;
+	return false;
 }
 
 void DrawMaze()
 {
-	cout << "  ";
+	printw("  ");
 	for(int x = 0; x < MAZE_X; x++) 
 	{
-		cout << " ";
-		cout << x;
+		printw(" %d", x);
 	}
-	cout << "\n";
+	printw("\n");
 	for(int y = 0; y < MAZE_Y; y++)
 	{
-		cout << y << " ";		
+		printw("%d ", y);		
 		for(int x = 0; x < MAZE_X; x++) 
 		{	
-			cout << " ";
-			cout << maze[y][x];
+			printw(" %c", maze[y][x]);
 		}
-		cout << "\n";	
+		printw("\n");
 	} 
+	refresh();
 }
 
 int main()
 {
-	if(InitialiseMaze() == -1) return 0;
-	int state = 1;
-	while(state == 1)
+	if(!InitialiseMaze()) return 0;
+	initscr();
+	raw();
+	noecho();
+	while(!CheckState())
 	{
-		system(CLEAR);
+		clear();
 		DrawMaze();
 		HandleInput();
-		state = CheckState();
 	}
-	system(CLEAR);
+	clear();
 	DrawMaze();
-	cout << "You won!\n";
+	endwin();
+	std::cout << "You won!\n";
+	return 1;
 }
